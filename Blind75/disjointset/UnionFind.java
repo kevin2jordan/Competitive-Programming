766785ebbc6132df347a5de2c00be41a5f8a5856
Rahul Kumar <r970ahul@gmail.com>
2023-03-05 15:21:08 +0530

    Update UnionFind.java

--- Blind75/disjointset/UnionFind.java
+++ Blind75/disjointset/UnionFind.java
@@ -1,36 +1,40 @@
-class UnionFind {
+class  UnionFind {
+    int v;
     int parents[];
     int rank[];
-    int V;
-    UnionFind(int v) {
-        this.V = v;
-        parents = new int[V];
-        rank = new int[V];
-
-        for(int i=0;i<V;i++) {
+    
+    UnionFind(int n) {
+        this.v = n;
+        parents = new int[v];
+        rank = new int[v];
+        for(int i=0;i<n;i++) {
             parents[i] = i;
             rank[i] = 1;
         }
     }
-
+    
+    
     void union(int x, int y) {
         int parentX = find(x);
         int parentY = find(y);
+        
         if(parentX != parentY) {
             if(rank[parentX] < rank[parentY]) {
-                parents[parentX] = parents[parentY];
+                parents[parentX] = parentY;
                 rank[parentX] += rank[parentY];
             } else {
-                parents[parentY] = parents[parentX];
-                rank[parentY] += rank[parentX];
+                parents[parentY] = parentX;
+                rank[parentY] += parentX;
             }
+            
         }
     }
-
+    
     int find(int x) {
         if(parents[x] != x) {
             parents[x] = find(parents[x]);
         }
+        
         return parents[x];
     }
 }
